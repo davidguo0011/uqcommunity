d282e1cbcb3f386deffb99c9ef1d97ada8d8d0d2
davidguo0011 <davidg9911mail@gmail.com>
2023-01-22 14:45:36 +0800

    topnavfrint topnavchat -> topnav

--- src/pages/ChatPage/ChatRoom/ChatRoom.js
+++ src/pages/ChatPage/ChatRoom/ChatRoom.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import styles from './ChatRoom.module.scss';
-import TopNavChat from '../TopNavChat/TopNavChat';
+import TopNav from '../TopNav/TopNav';
 import avatar from '../../../assets/discord.png';
 import ChatInput from '../../../components/Chat/ChatInput/ChatInput';
 import Message from '../../../components/Chat/Message/Message';
+import RightSideMenu from '../RightSideMenu/RightSideMenu';
 
 export default function ChatRoom({ chatName }) {
   return (
     <>
-      <TopNavChat chatName={chatName} />
-      <div className={styles.chatRoomContainer}>
-        <div className={styles.chatHeader}>
-          <img src={avatar} alt='' />
-          <h2>{chatName}</h2>
-          <div className={styles.buttonContainer}>
-            <button>删除好友</button>
-            <button>屏蔽</button>
+      <TopNav chatName={chatName} type='chat' />
+      <div className={styles.sideContainer}>
+        <div className={styles.chatRoomContainer}>
+          <div className={styles.chatHeader}>
+            <img src={avatar} alt='' />
+            <h2>{chatName}</h2>
+            <div className={styles.buttonContainer}>
+              <button>删除好友</button>
+              <button>屏蔽</button>
+            </div>
           </div>
-        </div>
-        <div className={styles.chatArea}>
-          <Message chatName={chatName} reverse={false} />
-          <Message chatName={chatName} reverse={false} />
+          <div className={styles.chatArea}>
+            <Message chatName={chatName} reverse={false} />
+            <Message chatName={chatName} reverse={false} />
 
-          <Message chatName={chatName} reverse />
-          <Message chatName={chatName} reverse />
-          <Message chatName={chatName} reverse={false} />
-          <Message chatName={chatName} reverse={false} />
-          <Message chatName={chatName} reverse />
-          <Message chatName={chatName} reverse />
+            <Message chatName={chatName} reverse />
+            <Message chatName={chatName} reverse />
+            <Message chatName={chatName} reverse={false} />
+            <Message chatName={chatName} reverse={false} />
+            <Message chatName={chatName} reverse />
+            <Message chatName={chatName} reverse />
+          </div>
+          <ChatInput chatName={chatName} />
         </div>
-        <ChatInput chatName={chatName} />
+        <RightSideMenu />
       </div>
     </>
   );
---
+++ src/pages/ChatPage/RightSideMenu/RightSideMenu.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import styles from './RightSideMenu.module.scss';
+export default function RightSideMenu() {
+  return <div className={styles.rightSideMenuContainer}></div>;
+}
